b6f57606cd0c4ae56d6e2d614e531cedd2f51cec
Roman <lytikroman5@gmail.com>
2022-09-01 19:26:07 +0200

    Fixed search page title problem

--- weather_app/lib/presentation/pages/search_page/search_page.dart
+++ weather_app/lib/presentation/pages/search_page/search_page.dart
@@ -116,6 +116,7 @@ class _SearchPageFormState extends State<_SearchPageForm> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 RichText(
+                  textAlign: TextAlign.left,
                   text: TextSpan(
                     children: const [
                       TextSpan(
